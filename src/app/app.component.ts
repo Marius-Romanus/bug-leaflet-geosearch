a179e74c419ff63d61bc0461f4d37c9cbb3c2dc1
Marius-Romanus <dejadme_el_silencio@hotmail.com>
2022-10-06 20:30:06 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'demo-leaflet-geosearch';
+}
